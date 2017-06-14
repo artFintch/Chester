0be8e33d50b59528eae2af8add7fecf952c3c54f
Khorkov Vyacheslav <artfintch@ya.ru>
2017-06-14 18:37:35 +0500

    Added extension for flattening query string.

---
+++ Chester/String+Query.swift
@@ -0,0 +1,16 @@
+//
+//  String+Query.swift
+//  Chester
+//
+//  Created by Vyacheslav Khorkov on 14/06/2017.
+//  Copyright © 2017 Vyacheslav Khorkov. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+	
+	var flatQuery: String {
+		return replacingOccurrences(of: "(\\n|  )*", with: "", options: .regularExpression)
+	}
+}
